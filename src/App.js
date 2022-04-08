5903238328ac590ed52e87c8f10958d8a78058d5
LongTran <phuclongtranphan112@gmail.com>
2022-04-08 20:29:24 +0700

    added display data

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 
+// EXERCISE:
+// Add Humidity, Min tempearature, and weather icons
+
 
 function App(){
   //useEffect hook tells our component app to do something
   // after rendering
+  const [search, setSearch] = useState('')
   const [allData, setAllData] = useState({
     city:'',
     country:'',
-    temperature:''
+    temperature:'',
+    airHumid:'',
+    tempMin:'',
+    icon:'',
+    description: ''
   })
 
   useEffect(() =>{
@@ -24,12 +32,17 @@ function App(){
     try{
     const APIKEY = '4fbc70ea7de61f44e79330f6546ebe46';
     // this is how to use axios
-    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${'rome'}&appid=${APIKEY}`);
+    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric`);
     
     await setAllData({
-      city: result,
+      city: result.data.name,
       country: result.data.sys.country,
-      temperature: result
+      temperature: result.data.main.temp,
+      airHumid: result.data.main.humidity,
+      tempMin: result.data.main.temp_min,
+      //watch out for array
+      icon: result.data.weather[0].icon,
+      description: result.data.weather[0].description
 
     })
   } catch(e){
@@ -37,11 +50,43 @@ function App(){
   }
   }
 
+  const changeHandler = (event) =>{
+    setSearch(event.target.value)
+    
+  }
+
+  const submitHandler = (event) =>{
+    console.log(search);
+    event.preventDefault();
+    fetchData(search)
+
+  }
+
   return( 
+    <main>
     <div className="App">
-      {console.log("testing 123", allData.country)}
+      <form onSubmit={submitHandler}>
+        <input value = {search} type = 'text' name ='city' placeholder='cityname' onChange={changeHandler}/>
+        <button for='city'>Search</button>
+      </form>
+
+      <section>
+        <img src={'http://openweathermap.org/img/wn/'+ allData.icon +'@2x.png'}/>
+        <h1>{allData.city}</h1>
+        <h1>{allData.country}</h1>
+        <h2>Temperature</h2>
+        <p>{allData.temperature}*C</p>
+        <h2>Air Humid</h2>
+        <p>{allData.airHumid}%</p>
+        <h2>Min Temperature</h2>
+        <p>{allData.tempMin}*C</p>
+        <h2>Description</h2>
+        <p>{allData.description}</p>
+      
+      </section>
 
     </div>
+    </main>
   );
 }
 
